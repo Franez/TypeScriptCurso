4a74a1dac785f171cc09398d82d33d948f74f0be
INVESTIGACIONES\flopezm <flopezm>
2023-01-04 11:09:22 -0300

    avance de clase 9 a clase 16

--- bases/app.js
+++ bases/app.js
@@ -1,3 +1,26 @@
 "use strict";
-const msg = 'Hola Mundo';
-console.log(msg);
+(() => {
+    // Clase 1
+    const msg = 'Hola Mundo';
+    const hero = {
+        name: 'iron man',
+        age: 45
+    };
+    hero.age = 50;
+    console.log(hero.name, hero.age + 1);
+    console.log(msg);
+    // Clase 2
+    const a = 10; //constante , no cambia su valor
+    let b = 10; //let crea variable que puede cambiar su valor peor no su tipo
+    const c = 10; //lo ideal es declarar el tipo de dato, esto limita los valores que puede tener la variable o constante
+    let d; //como indica any puede ser cualquir tipo de dato por ejempo:
+    d = 3.14; //un double
+    d = true; //un booleano
+    d = {}; //un objeto
+    d = []; //un arreglo
+    function sayHello1(msg) {
+        const e = 'Mi edad es: ';
+        console.log(e, msg);
+    }
+    sayHello1(25);
+})(); //para correr este archivo invocar se desde el index
